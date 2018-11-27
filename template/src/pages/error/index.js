ea0b2f2d5ffdba63b43cd97797119b6d773e6817
兰晶 <1530562965@qq.com>
2018-11-27 17:25:36 +0800

    提交代码

---
+++ template/src/pages/error/index.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import {connect} from 'dva';
+import Link from 'umi/link';
+import Exception from 'components/Exception';
+import qs from 'qs';
+class Error extends Component {
+	constructor(props, context) {
+		super(props, context);
+	}
+	
+	render() {
+		const {location} = this.props;
+    let {urldata}=location;
+    if (!urldata){
+      urldata=qs.parse(location.search,{ignoreQueryPrefix:true})
+    }
+		return (
+      <Exception
+      type={urldata.msg}
+      style={{ minHeight: 500, height: '80%' }}
+      linkElement={Link}
+    />
+		)
+	}
+	
+}
+
+export default connect(({error}) => ({
+	error,
+}))(Error);
