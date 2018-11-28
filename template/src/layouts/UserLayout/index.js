45bde950d9d4a13ba3ff2efe29f09431b90d2630
兰晶 <1530562965@qq.com>
2018-11-28 09:27:44 +0800

    更新代码

--- template/src/layouts/UserLayout/index.js
+++ template/src/layouts/UserLayout/index.js
@@ -8,7 +8,7 @@ import loginTitleImg from 'assets/loginTitle.png';
 
 const links = [];
 const copyright = (<Fragment>
-  <span style={{color: '#fff'}}
+  <span className={styles.footspan}
   >
     福建博思软件<br/>
     闽ICP备17021229号 客服电话：400-877-1666 服务时间：9:00-17:30
