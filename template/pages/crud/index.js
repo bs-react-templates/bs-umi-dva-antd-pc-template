3155a64b0261fceb837cf940fe8467ee10c6b9ee
兰晶 <1530562965@qq.com>
2018-12-03 16:00:16 +0800

    更新代码

--- template/pages/crud/index.js
+++ template/pages/crud/index.js
@@ -1,1 +1,14 @@
-import '@babel/polyfill'
+import React, { Component } from 'react';
+import router from 'umi/router';
+export default class AnalysedOrder extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+    render(){
+        return(
+        <div>这是增删改查模块</div>
+        )
+    }
+}
