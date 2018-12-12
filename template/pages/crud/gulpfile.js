6674e4623e5464304b05cf7661209ee33b633a97
兰晶 <1530562965@qq.com>
2018-12-12 14:34:27 +0800

    更新代码

---
+++ template/pages/crud/gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+var gulp_rename = require('gulp-rename');
+var gulp_ejs = require('gulp-ejs');
+const exec = require('child_process').exec;
+var clean = require('gulp-clean');
+const path = require('path');
+const src = path.join(__dirname);
+gulp.task('gulptask', (cb) => {
+  return exec(`genercode --config ${__dirname}/config.json`, (err, stdout, stderr) => {
+    if (err) {return err}
+    if(!stdout){
+      return;
+    }
+    var stdoutObj = JSON.parse(stdout);
+    gulp
+    .src([src + '/**/*.ejs'])
+    .pipe(gulp_ejs(stdoutObj))
+    .pipe(clean({force: true}))
+    .pipe(
+			gulp_rename(path => {
+				path.extname = '.js';
+			})
+    )
+    .pipe(gulp.dest(src))
+  })
+})
+gulp.task('default', ['gulptask'])
